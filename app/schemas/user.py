a2c2810ece3e9844b165c2a015692163ec858360
zradj <zradj7@gmail.com>
2025-12-12 04:44:52 +0400

    use uuid instead of plain int

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,7 +1,9 @@
+import uuid
+
 from sqlmodel import SQLModel, Field
 
 class User(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     username: str
     password: str
     display_name: str | None = None
